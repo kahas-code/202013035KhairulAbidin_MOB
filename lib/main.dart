726ad39260a4606119ba55eaae23ba99280cf90a
kahas-code <80758150+kahas-code@users.noreply.github.com>
2021-10-13 20:39:27 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,46 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart'; //mengimport apa yang ada didalam class yang telah dibuat
 
-class Tampilan extends StatelessWidget {
+class LayoutRow extends StatelessWidget {
+  //untuk membuat nama class,dalam pembuatan nama class,
+  //dalam pembuatan nama class harus menggunakna huruf kapital
+  const LayoutRow({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
+    // kelas yang membuat widget yang sifatnya statis
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Sample Code'),
+      //untuk memuat fungsi dari widget seperti title dan lainnya.
+      home: Scaffold(
+        //Widget utama untuk membuat sebuah halaman pafa flutter
+        // ignore: avoid_unnecessary_containers
+        body: Container(
+          child: Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Nasi Goreng"),
+                ],
+              ),
+              Spacer(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'lib/assets/gambar/nasigoreng.jpg',
+                    width: 300,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(child: Text('Hallo')),
-      backgroundColor: Colors.blueGrey.shade200,
-    ));
+    );
   }
 }
 
 void main() {
-  runApp(Tampilan());
+  runApp(LayoutRow());
 }
